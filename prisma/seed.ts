7cf418bef164a0b7b8befd28916f70061cf39bd2
devluizfillipe-eds <devluizfillipe@gmail.com>
2025-12-16 23:24:28 -0300

    feat: modificações de cpmpatibilidade com o supabase

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import { PrismaClient, Role } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import * as bcrypt from 'bcrypt';
 
-const prisma = new PrismaClient({
-  // @ts-ignore
-  datasourceUrl: process.env.DATABASE_URL,
-});
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   const password = await bcrypt.hash('admin123', 10);
